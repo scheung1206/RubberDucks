8be5cbeb0c521fdce2f7eeb5d20c8a88a5450c26
Stephen Cheung <stephencheung@Stephens-MacBook-Pro.local>
2016-03-24 11:56:34 -0700

    Update 3-24-16

--- Calendar/app/controllers/calendar_controller.rb
+++ Calendar/app/controllers/calendar_controller.rb
@@ -1,5 +1,8 @@
 class CalendarController < ApplicationController
+
   def show
-     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @events = Event.all
+    @events_by_date = @events.group_by(&:date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 end

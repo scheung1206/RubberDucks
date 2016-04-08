9a9a0fa5c438fa274e2e1b39e643907b440f001d
Stephen Cheung <stephencheung@Stephens-MacBook-Pro.local>
2016-04-08 02:23:40 -0700

    Post/Edit/Delete

--- Calendar/app/controllers/posts_controller.rb
+++ Calendar/app/controllers/posts_controller.rb
@@ -2,20 +2,25 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    @event = Event.find(params[:event_id])
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+    @event = Event.find(params[:event_id])
   end
 
   # GET /posts/1/edit
@@ -38,10 +43,12 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    #@event = Event.find(params[:event_id])
+    #@post = Post.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to @event, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -52,9 +59,11 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    @event = Event.find(params[:event_id])
+    @post = @event.posts.find(params[:id])
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to @event, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -63,6 +72,11 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+      #@event = Event.find(params[:event_id])#
+    end
+
+    def set_event
+    @event = Event.find(params[:event_id])#
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- Calendar/db/schema.rb
+++ Calendar/db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160303043632) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "event_id"
   end
 
   create_table "users", force: :cascade do |t|
